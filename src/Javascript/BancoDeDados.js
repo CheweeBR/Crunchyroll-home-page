f44ce4cc936997cd4ebe4e1b65616d34c1c8bdb4
Tiago Eloy Possidonio <tiagoeloy@alunos.utfpr.edu.br>
2023-06-29 15:31:06 -0300

    implementação da função de busca

--- src/Javascript/BancoDeDados.js
+++ src/Javascript/BancoDeDados.js
@@ -15,14 +15,44 @@ class BancoDeDados {
 
     lista.push(anime);
     localStorage.Lista = JSON.stringify(lista);
-    console.log("Anime cadastrado");
+    alert("Anime cadastrado com sucesso!");
     console.log(lista.length);
     for (let c = 0; c < lista.length; c++) {
       console.log(lista[c]);
     }
   }
+
+  Buscar(Nome) {
+    if (localStorage.lista) {
+      const lista = JSON.parse(localStorage.getItem("Lista"));
+      const resultados = lista.filter((anime) =>
+        anime.nome.toLowerCase().includes(Nome.toLowerCase())
+      );
+      if (resultados.length > 0) {
+        console.log(`Resultados da busca para "${Nome}":`);
+        resultados.forEach((anime) => {
+          console.log(`Título: ${anime.nome}`);
+          console.log(`Descrição: ${anime.descricao}`);
+          console.log(`Data: ${anime.data}`);
+          console.log(`Imagem: ${anime.imagem}`);
+          console.log("---");
+        });
+      } else {
+        console.log(`Nenhum resultado encontrado para "${Nome}".`);
+      }
+    } else {
+      console.log("Nenhum anime cadastrado.");
+    }
+  }
 }
+
 function Cadastrar() {
   const bd = new BancoDeDados();
   bd.Cadastrar();
+}
+
+function Buscar() {
+  let Nome = document.getElementById("nome").value;
+  const bd = new BancoDeDados();
+  bd.Buscar(Nome);
 }
